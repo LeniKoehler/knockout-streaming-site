f4b352b9684e4b156a36fb4b9a7e21ec410aaf59
LeniKoehler <maria_m_koehler@hotmail.de>
2021-06-13 11:50:51 +0200

    feat(moviesList): added knockout data bind

--- js/viewModel.js
+++ js/viewModel.js
@@ -20,16 +20,38 @@
          
                 // document.getElementById("box1").style.display = "block";
          
-             }
+            }
          
              this.closeNav = function() {
                 document.getElementById("mySidenav").style.display = "none";
                 document.getElementById("box1").style.transform = "scale(1)";
                 document.getElementById("box1").style.transition = "all 0.7s ease";
                 document.getElementById("box1").style.border = "none";
-             }
+            }
+            
+
+            var self = this;
+                
+            // Editable data
+            self.movies = ko.observableArray([
+                new ListEntry("Titanic", "James Cameron"),
+                new ListEntry("The Dark Knight Rises", "Christopher Nolan")
+            ]);
+            
+            // Operations
+            self.addMovie = function() {
+                self.movies.push(new ListEntry("testTitel", "testRegisseur"));
+            }
+            self.removeMovie = function(movie) { self.movies.remove(movie) }
+         
 
         }
+
         // Activates knockout.js
         ko.applyBindings(new AppViewModel());
         
+        function ListEntry(title, regisseur) {
+            var self = this;
+            self.title = title;
+            self.regisseur = regisseur;
+        }
